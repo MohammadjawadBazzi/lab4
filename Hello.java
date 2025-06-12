1d850a09fb04fed7d35e44e64b8b6b0fdd2261fa
Mohammadjawad Bazzi <mohammadjawadbazzi@gmail.com>
2025-06-12 10:01:30 -0400

    Triggering Jenkins

--- Hello.java
+++ Hello.java
@@ -2,10 +2,10 @@ public class Hello {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
             System.out.println("Hello World..." + i);
+            System.out.println("This is a new feature branch edit.");
+            System.out.println("Ready for Jenkins!");
         }
     }
-    System.out.println("This is a new feature branch edit.");
-    System.out.println("Ready for Jenkins!");
 }
 
 
